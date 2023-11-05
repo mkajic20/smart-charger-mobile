dfd5e4f4295108b726d9e3ad01a157e5dfb59024
Tin <55455483+TinSisko-gameDev@users.noreply.github.com>
2023-11-05 23:05:59 +0100

    Implemented design of home page.

--- Software/app/build.gradle.kts
+++ Software/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "org.foi.air.smartcharger"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "org.foi.air.smartcharger"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
