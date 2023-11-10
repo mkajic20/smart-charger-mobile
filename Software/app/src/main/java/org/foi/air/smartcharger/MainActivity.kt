ee6d11761e9d70c21c84e2c1321038d7a4e1014c
Tin <55455483+TinSisko-gameDev@users.noreply.github.com>
2023-11-10 23:28:00 +0100

    Added View Binding, fixed variable naming convention, and changed string literals into string resources

--- Software/app/src/main/java/org/foi/air/smartcharger/MainActivity.kt
+++ Software/app/src/main/java/org/foi/air/smartcharger/MainActivity.kt
@@ -2,33 +2,35 @@ package org.foi.air.smartcharger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import androidx.core.content.ContextCompat
+import org.foi.air.smartcharger.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.button1)
-        val status = findViewById<TextView>(R.id.status)
-        val instructions = findViewById<TextView>(R.id.instructions)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //var that holds connection button state
         var state = true
-        button.setOnClickListener()
+        binding.btnConnectionButton.setOnClickListener()
         {
+            //If button has "Connect" text on it
             if (state) {
-                button.setBackground(resources.getDrawable(R.drawable.custom_buttonbackground_cancel))
-                button.text = "Cancel"
-                status.text = "Status: Waiting for card..."
-                instructions.text =
-                    "Please put your RFID card on the phone screen to connect with the charger"
+                binding.btnConnectionButton.background = ContextCompat.getDrawable(this, R.drawable.custom_buttonbackground_cancel)
+                binding.btnConnectionButton.text = resources.getString(R.string.cancel_button_text)
+                binding.tvStatus.text = resources.getString(R.string.status_waiting_card)
+                binding.tvInstructions.text = resources.getString(R.string.instructions_rfid_connection)
                 state = false
 
-            } else {
-                button.setBackground(resources.getDrawable(R.drawable.custom_button_background))
-                button.text = "Connect"
-                instructions.text =
-                    "Before you start using charger you will have to connect with your RFID card."
-                status.text = ""
+            }
+            //If button has "Cancel" text on it
+            else {
+                binding.btnConnectionButton.background = ContextCompat.getDrawable(this, R.drawable.custom_buttonbackground_connect)
+                binding.btnConnectionButton.text = resources.getString(R.string.connect_button_text)
+                binding.tvInstructions.text = resources.getString(R.string.instructions_before_connection)
+                binding.tvStatus.text = ""
                 state = true
 
             }
