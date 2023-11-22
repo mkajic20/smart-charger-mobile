c655941ca2f05ea4e771414a877560430863674b
Tin <55455483+TinSisko-gameDev@users.noreply.github.com>
2023-11-22 18:13:08 +0100

    Changed string literals into string resources

--- Software/app/src/main/java/org/foi/air/smartcharger/LoginActivity.kt
+++ Software/app/src/main/java/org/foi/air/smartcharger/LoginActivity.kt
@@ -9,7 +9,7 @@ import org.foi.air.api.request_handlers.LoginRequestHandler
 import org.foi.air.core.models.ErrorResponseBody
 import org.foi.air.core.models.SuccessfulLoginResponseBody
 import org.foi.air.smartcharger.databinding.ActivityLoginBinding
-import org.foi.air.smartcharger.databinding.ActivityMainBinding
+
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding : ActivityLoginBinding
@@ -18,7 +18,7 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
         //setContentView(R.layout.activity_login)
-        binding.btnLogin.setOnClickListener(){
+        binding.btnLogin.setOnClickListener{
             val loginBody = LoginBody(
                 binding.txtEmail.text.toString(),
                 binding.txtPassword.text.toString()
@@ -36,8 +36,8 @@ class LoginActivity : AppCompatActivity() {
             override fun onSuccessfulResponse(response: SuccessfulLoginResponseBody) {
                 Log.i("login","I logged in and imagine this is new page")
                 Log.i("login","And i have this data: "+response.user+" with this jwt: "+response.jwt)
-                binding.tvEmailError.text = "Correct"
-                binding.tvPasswordError.text = "Correct"
+                binding.tvEmailError.text = resources.getString(R.string.login_succeeded)
+                binding.tvPasswordError.text = resources.getString(R.string.login_succeeded)
             }
 
             override fun onErrorResponse(response: ErrorResponseBody) {
@@ -59,30 +59,30 @@ class LoginActivity : AppCompatActivity() {
     private fun errorResponse(error : String){
         when (error) {
             "Invalid credentials." -> {
-                binding.tvPasswordError.text = "You have entered the wrong password"
+                binding.tvPasswordError.text = resources.getString(R.string.wrong_password)
                 binding.tvEmailError.text = ""
 
             }
             "This account is disabled." ->{
-                binding.tvEmailError.text = "This account is disabled."
+                binding.tvEmailError.text = resources.getString(R.string.disabled_account)
                 binding.tvPasswordError.text = ""
             }
             "This email is not registered." ->{
-                binding.tvEmailError.text = "This email is not registered."
+                binding.tvEmailError.text = resources.getString(R.string.unregistered_email)
                 binding.tvPasswordError.text = ""
             }
 
             "Email is not valid." ->{
-                binding.tvEmailError.text = "Email is not valid."
+                binding.tvEmailError.text = resources.getString(R.string.invalid_email)
                 binding.tvPasswordError.text = ""
             }
             "Password must have at least 6 characters." ->{
-                binding.tvPasswordError.text = "Password must have at least 6 characters."
+                binding.tvPasswordError.text = resources.getString(R.string.short_password)
                 binding.tvEmailError.text = ""
             }
             else -> {
-                binding.tvEmailError.text = "Something went wrong..."
-                binding.tvPasswordError.text = "Something went wrong..."
+                binding.tvEmailError.text = resources.getString(R.string.unexpected_login_error)
+                binding.tvPasswordError.text = resources.getString(R.string.unexpected_login_error)
             }
         }
     }
