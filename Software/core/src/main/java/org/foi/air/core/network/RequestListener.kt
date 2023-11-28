74bf2573b94e80cd9dc622a8d117e2b33ace8e43
Tin <55455483+TinSisko-gameDev@users.noreply.github.com>
2023-11-28 18:32:11 +0100

    Fixed conflicts between login and develop branches.

--- Software/core/src/main/java/org/foi/air/core/network/RequestListener.kt
+++
@@ -1,7 +0,0 @@
-import org.foi.air.core.models.ErrorResponseBody
-
-interface ResponseListener<T> {
-    fun onSuccessfulResponse(response: T)
-    fun onErrorResponse(response: ErrorResponseBody)
-    fun onApiConnectionFailure(t: Throwable)
-}
