0a93a999cf1257226144e408da49f0cb540398fd
Tin <55455483+TinSisko-gameDev@users.noreply.github.com>
2023-12-07 11:11:40 +0100

    [SC-123] Implemented API get all cards for logged in user in RfidListFragment.

--- Software/app/src/main/java/org/foi/air/smartcharger/fragments/RfidListFragment.kt
+++ Software/app/src/main/java/org/foi/air/smartcharger/fragments/RfidListFragment.kt
@@ -1,24 +1,28 @@
 package org.foi.air.smartcharger.fragments
 
+import ResponseListener
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import org.foi.air.api.network.ApiService
+import org.foi.air.api.request_handlers.GetRfidCardsForUserRequestHandler
+import org.foi.air.core.data_classes.RfidCard
+import org.foi.air.core.models.ErrorResponseBody
+import org.foi.air.core.models.RfidCardResponseBody
 import org.foi.air.smartcharger.R
 import org.foi.air.smartcharger.context.Auth
-import org.foi.air.smartcharger.context.RfidCardModel
+import org.foi.air.smartcharger.context.CardBodyModel
 import org.foi.air.smartcharger.context.RfidCardRecyclerAdapter
 import org.foi.air.smartcharger.databinding.FragmentRfidListBinding
 
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RfidListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class RfidListFragment : Fragment() {
 
     private lateinit var binding: FragmentRfidListBinding
@@ -29,36 +33,69 @@ class RfidListFragment : Fragment() {
     }
 
     private lateinit var newRecyclerView : RecyclerView
-    private lateinit var rfidCardList : ArrayList<RfidCardModel>
-    lateinit var rfidName : Array<String>
-    lateinit var rfidState : Array<String>
+    private lateinit var rfidCardList : ArrayList<CardBodyModel>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         binding = FragmentRfidListBinding.inflate(inflater,container,false)
+        ApiService.authToken = Auth.jwt!!
 
-        rfidName = resources.getStringArray(R.array.rfid_cards_names)
-        rfidState = resources.getStringArray(R.array.rfid_cards_states)
+        //get all cards for logged user
+        getAllCards()
+        binding.btnRetryConnection.setOnClickListener{
+            getAllCards()
+            Log.i("rfid", "click")
+        }
 
         newRecyclerView = binding.rwRfidCards
         newRecyclerView.layoutManager = LinearLayoutManager(context)
 
-        rfidCardList = arrayListOf<RfidCardModel>()
-        getRfidCardsData()
+        rfidCardList = arrayListOf<CardBodyModel>()
 
         return binding.root
     }
 
-    private fun getRfidCardsData() {
-        for(i in rfidName.indices){
-            val rfidCard = RfidCardModel(rfidName[i],rfidState[i].toBoolean())
-            rfidCardList.add(rfidCard)
-        }
-        newRecyclerView.adapter = RfidCardRecyclerAdapter(rfidCardList)
+    private fun getAllCards(){
+        val getRfidCardsForUserRequestHandler = GetRfidCardsForUserRequestHandler(Auth.userId!!.toInt())
+        binding.tvServerError.visibility = View.INVISIBLE
+        binding.btnRetryConnection.visibility = View.INVISIBLE
+        getRfidCardsForUserRequestHandler.sendRequest(object: ResponseListener<RfidCardResponseBody>{
+
+            override fun onSuccessfulResponse(response: RfidCardResponseBody) {
+                getRfidCardsData(response.rfidCardList)
+            }
+
+            override fun onErrorResponse(response: ErrorResponseBody) {
+                val toast = Toast.makeText(this@RfidListFragment.context, response.message, Toast.LENGTH_LONG)
+                toast.show()
+            }
+
+            override fun onApiConnectionFailure(t: Throwable) {
+                val toast = Toast.makeText(this@RfidListFragment.context, resources.getString(R.string.cant_reach_server), Toast.LENGTH_LONG)
+                toast.show()
+                binding.tvServerError.visibility = View.VISIBLE
+                binding.btnRetryConnection.visibility = View.VISIBLE
+            }
+
+
+        })
     }
 
 
+    private fun getRfidCardsData(rfidCardList: MutableList<RfidCard>) {
+        this.rfidCardList.clear()
+        for(i in rfidCardList.indices){
+            val rfidCard = CardBodyModel(rfidCardList[i].name,rfidCardList[i].active.toString(), rfidCardList[i].id)
+            this.rfidCardList.add(rfidCard)
+        }
+        newRecyclerView.adapter = RfidCardRecyclerAdapter(this.rfidCardList, getContext(), this)
+    }
+
+
+
+
 }
+
