62ea0cf1b0f19df4354902653e4727807412d04e
Tin <55455483+TinSisko-gameDev@users.noreply.github.com>
2023-12-08 13:15:27 +0100

    [SC-124] Implemented API delete selected card through recycler view adapter.

--- Software/app/src/main/java/org/foi/air/smartcharger/context/RfidCardRecyclerAdapter.kt
+++ Software/app/src/main/java/org/foi/air/smartcharger/context/RfidCardRecyclerAdapter.kt
@@ -1,41 +1,39 @@
 package org.foi.air.smartcharger.context
 
-import android.util.Log
+import ResponseListener
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
-import android.widget.Switch
 import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import org.foi.air.api.request_handlers.DeleteCardRequestHandler
+import org.foi.air.core.models.ErrorResponseBody
+import org.foi.air.core.models.ResponseBody
 import org.foi.air.smartcharger.R
+class RfidCardRecyclerAdapter(private val rfidCardList: ArrayList<CardBodyModel>,private val context: Context?) : RecyclerView.Adapter<RfidCardRecyclerAdapter.MyViewHolder>() {
 
-class RfidCardRecyclerAdapter(private val rfidCardList : ArrayList<RfidCardModel>) : RecyclerView.Adapter<RfidCardRecyclerAdapter.MyViewHolder>() {
-
-
-
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.rfid_card_element,parent, false)
         return MyViewHolder(itemView)
     }
-
 
     var lastSelected : Int? = null
     var lastHolder : MyViewHolder? = null
 
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val currentItem = rfidCardList[position]
+        val currentItem = rfidCardList[holder.adapterPosition]
 
         holder.tvRfidCardName.text = currentItem.name
-        holder.swRfidCardSwitch.isChecked = currentItem.active
-
+        holder.tvRfidCardState.text = currentItem.active
 
         //because of view reusability this if else must be here
-        if(lastSelected == position){
+        if(lastSelected == holder.adapterPosition){
             holder.card.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.black ))
             holder.btnDelete.visibility = View.VISIBLE
         }else{
@@ -43,20 +41,16 @@ class RfidCardRecyclerAdapter(private val rfidCardList : ArrayList<RfidCardModel
             holder.btnDelete.visibility = View.INVISIBLE
         }
 
-        holder.swRfidCardSwitch.setOnClickListener{
-
-        }
-
-        holder.card.setOnClickListener{
+        holder.card.setOnLongClickListener{
             if(holder.btnDelete.visibility == View.INVISIBLE){
                 if(lastSelected == null) {
-                    lastSelected = position
+                    lastSelected = holder.adapterPosition
                     lastHolder = holder
                 }
                 else{
                     lastHolder?.card?.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.rfidCardColor ))
                     lastHolder?.btnDelete?.visibility = View.INVISIBLE
-                    lastSelected = position
+                    lastSelected = holder.adapterPosition
                     lastHolder = holder
                 }
                 holder.card.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.black ))
@@ -65,57 +59,42 @@ class RfidCardRecyclerAdapter(private val rfidCardList : ArrayList<RfidCardModel
                 holder.card.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.rfidCardColor ))
                 holder.btnDelete.visibility = View.INVISIBLE
             }
-            //return@setOnLongClickListener true
+            return@setOnLongClickListener true
         }
         holder.btnDelete.setOnClickListener{
-            Log.i("rfid", "delete $position")
-            rfidCardList.removeAt(position)
-            notifyItemRemoved(position)
-            notifyItemRangeChanged(position, itemCount)
-            lastSelected = null
-            lastHolder = null
+
+            val deleteCardRequestHandler = DeleteCardRequestHandler(Auth.userId!!.toInt(), currentItem.Id)
+
+            deleteCardRequestHandler.sendRequest(object: ResponseListener<ResponseBody>{
+                override fun onSuccessfulResponse(response: ResponseBody) {
+                    val toast = Toast.makeText(context, response.message, Toast.LENGTH_SHORT)
+                    toast.show()
+                    rfidCardList.removeAt(holder.adapterPosition)
+                    notifyItemRemoved(holder.adapterPosition)
+                    notifyItemRangeChanged(holder.adapterPosition, itemCount)
+                    lastSelected = null
+                    lastHolder = null
+
+                }
+
+                override fun onErrorResponse(response: ErrorResponseBody) {
+                    val toast = Toast.makeText(context, response.message, Toast.LENGTH_SHORT)
+                    toast.show()
+                }
+
+                override fun onApiConnectionFailure(t: Throwable) {
+                    val toast = Toast.makeText(context, R.string.cant_reach_server, Toast.LENGTH_SHORT)
+                    toast.show()
+                }
+
+            })
+
+
+
         }
 
 
     }
-
-/*
-    var Selected : Int? = null
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val currentItem = rfidCardList[position]
-        holder.tvRfidCardName.text = currentItem.name
-        holder.swRfidCardSwitch.isChecked = currentItem.active
-
-        if(Selected == holder.adapterPosition) {
-            holder.card.setCardBackgroundColor(
-                ContextCompat.getColor(
-                    holder.itemView.context,
-                    R.color.black
-                )
-            )
-        }
-        else
-            holder.card.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.rfidCardColor))
-
-        holder.card.setOnClickListener{
-            Selected = position
-            if(Selected == holder.adapterPosition) {
-                holder.card.setCardBackgroundColor(
-                    ContextCompat.getColor(
-                        holder.itemView.context,
-                        R.color.black
-                    )
-                )
-            }
-            else
-                holder.card.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.rfidCardColor))
-    }
-    }
-
-    public fun selectItem(position : Int){
-        lastSelected = position
-    }
-*/
 
 
     override fun getItemCount(): Int {
@@ -125,11 +104,8 @@ class RfidCardRecyclerAdapter(private val rfidCardList : ArrayList<RfidCardModel
     class MyViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
 
         val tvRfidCardName : TextView = itemView.findViewById(R.id.tvRfidCardName)
-        val swRfidCardSwitch : Switch = itemView.findViewById(R.id.swRfidCardSwitch)
+        val tvRfidCardState : TextView = itemView.findViewById(R.id.tvRfidCardState)
         val btnDelete : ImageButton = itemView.findViewById(R.id.btnDelete)
         val card : CardView = itemView.findViewById(R.id.cvCard)
-        //val swRfidCardSwitch : androidx.appcompat.widget.SwitchCompat = itemView.findViewById(R.id.swRfidCardSwitch)
 
-    }
-
-}
+}}
