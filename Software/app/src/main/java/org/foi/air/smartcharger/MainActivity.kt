3cb3bfc190c6f12671bc1c27195d00f0a61e8912
irosic20 <irosic20@student.foi.hr>
2023-12-12 17:37:04 +0100

    SC-120 Added methods into MainActivity.kt needed for executing intent of a scanned RFID card

--- Software/app/src/main/java/org/foi/air/smartcharger/MainActivity.kt
+++ Software/app/src/main/java/org/foi/air/smartcharger/MainActivity.kt
@@ -1,13 +1,16 @@
 package org.foi.air.smartcharger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.foi.air.core.interfaces.OnNewIntentListener
 import org.foi.air.smartcharger.databinding.ActivityMainBinding
 import org.foi.air.smartcharger.fragments.HomepageFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private var mOnNewIntentListener: OnNewIntentListener? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -17,5 +20,14 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.cvFragment, HomepageFragment())
                 .commit()
         }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        mOnNewIntentListener?.newIntent(intent)
+    }
+
+    fun setOnNewIntentListener(onNewIntentListener: OnNewIntentListener?){
+        mOnNewIntentListener = onNewIntentListener
     }
 }
