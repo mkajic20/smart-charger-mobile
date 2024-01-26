c6b7d23a36ab5adab95e46a84bb9e8fbf75d3edf
irosic20 <irosic20@student.foi.hr>
2024-01-26 20:24:51 +0100

    SC-245 Written UI test

--- Software/app/src/androidTest/java/org/foi/air/smartcharger/UITests.kt
+++ Software/app/src/androidTest/java/org/foi/air/smartcharger/UITests.kt
@@ -1,18 +1,14 @@
 package org.foi.air.smartcharger
 
-import androidx.fragment.app.testing.FragmentScenario
-import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.lifecycle.Lifecycle
+import android.os.SystemClock
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.uiautomator.By
-import androidx.test.uiautomator.UiDevice
-import org.foi.air.smartcharger.fragments.LoginFragment
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,17 +17,71 @@ import org.junit.Assert.*
 
 @RunWith(AndroidJUnit4::class)
 class UITests {
-    private lateinit var loginFragment: FragmentScenario<LoginFragment>
 
     @Test
-    fun wrongLogin_applicationWontLoginUser(){
-        val uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
-        loginFragment = launchFragmentInContainer()
-        loginFragment.moveToState(Lifecycle.State.STARTED)
-
+    fun navigateToLoginFragment(){
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        //Open hamburger menu
+        onView(withId(R.id.ivHamburger)).perform(click())
+        onView(withId(R.id.loginItem)).perform(click())
+        //Check if in login fragment
+        onView(withId(R.id.bgLoginWave)).check(matches(isDisplayed()))
+    }
+    @Test
+    fun unsuccessfulLogin(){
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        //Open hamburger menu
+        onView(withId(R.id.ivHamburger)).perform(click())
+        onView(withId(R.id.loginItem)).perform(click())
+        //Login fragment
         onView(withId(org.foi.air.login_email_password.R.id.txtEmail)).perform(typeText("admin@gmail.com"))
         Espresso.closeSoftKeyboard()
         onView(withId(org.foi.air.login_email_password.R.id.btnLogin)).perform(click())
-        assertTrue(uiDevice.hasObject(By.displayId(R.id.tvMainHeader)))
+        //Check if still in login fragment
+        onView(withId(R.id.bgLoginWave)).check(matches(isDisplayed()))
     }
+
+    @Test
+    fun successfulLogin(){
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        //Open hamburger menu
+        onView(withId(R.id.ivHamburger)).perform(click())
+        onView(withId(R.id.loginItem)).perform(click())
+        //Login fragment
+        onView(withId(org.foi.air.login_email_password.R.id.txtEmail)).perform(typeText("admin@gmail.com"))
+        onView(withId(org.foi.air.login_email_password.R.id.txtPassword)).perform(typeText("test123"))
+        Espresso.closeSoftKeyboard()
+        onView(withId(org.foi.air.login_email_password.R.id.btnLogin)).perform(click())
+        //Wait for login to process
+        SystemClock.sleep(2000)
+        onView(withId(R.id.tvMainTitleRfidList)).check(matches(isDisplayed()))
+        //Logout
+        onView(withId(R.id.ivHamburger)).perform(click())
+        onView(withId(R.id.logoutItem)).perform(click())
+    }
+
+    @Test
+    fun navigateToChargingHistory(){
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        //Open hamburger menu
+        onView(withId(R.id.ivHamburger)).perform(click())
+        onView(withId(R.id.loginItem)).perform(click())
+        //Login fragment
+        onView(withId(org.foi.air.login_email_password.R.id.txtEmail)).perform(typeText("admin@gmail.com"))
+        onView(withId(org.foi.air.login_email_password.R.id.txtPassword)).perform(typeText("test123"))
+        Espresso.closeSoftKeyboard()
+        onView(withId(org.foi.air.login_email_password.R.id.btnLogin)).perform(click())
+        //Wait for login to process
+        SystemClock.sleep(2000)
+        //Navigate to charging history fragment
+        onView(withId(R.id.ivHamburger)).perform(click())
+        onView(withId(R.id.chargingHistoryItem)).perform(click())
+        //Wait for fragment to load
+        SystemClock.sleep(2000)
+        onView(withId(R.id.chargerNameTitle)).check(matches(isDisplayed()))
+        //Logout
+        onView(withId(R.id.ivHamburger)).perform(click())
+        onView(withId(R.id.logoutItem)).perform(click())
+    }
+
 }
